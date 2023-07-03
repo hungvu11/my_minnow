1aeea90a0144a2b652a8731ee44953910d94dc53
Hung Vu <vungochung96@gmail.com>
2023-07-03 22:32:45 +0700

    fix an error with bytes_pending

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -1,5 +1,5 @@
 #include "reassembler.hh"
-#include <limits.h>
+#define low_bound -300
 
 using namespace std;
 
@@ -8,11 +8,11 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
   // Your code here.
   uint64_t len = data.length();
   if (size_ <= first_index + len) {
-    for (uint64_t i=size_; i < first_index + len + 10; i++) {
-      char_pending.push_back(INT_MIN);
+    for (uint64_t i=size_; i < first_index + len + 1; i++) {
+      char_pending.push_back(low_bound);
       is_last.push_back(false);
     }
-    size_ = first_index + len + 10;
+    size_ = first_index + len + 1;
   }
 
   if (output.available_capacity() > 0 && first_index <= next_byte && next_byte < first_index + len) {
@@ -21,7 +21,7 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
     uint64_t start = next_byte-first_index;
     for (uint64_t i = start; i < len; i++) {
       if (capacity_ == 0) break;
-      if (char_pending[i+first_index] != INT_MIN) byte_pending--;
+      if (char_pending[i+first_index] != low_bound) byte_pending--;
       char_pending[i+first_index] = data[i];
       s += data[i];
       next_byte++;
@@ -31,7 +31,7 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
     
     // uint64_t start = next_byte;
     
-    while(char_pending[next_byte] != INT_MIN && capacity_ > 0) {
+    while(char_pending[next_byte] != low_bound && capacity_ > 0) {
       if (next_byte >= first_index + len) byte_pending--;
       s += char_pending[next_byte++];
       capacity_--;
@@ -41,8 +41,8 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
 
     if (is_last[next_byte-1]) output.close();
   } else if (output.available_capacity() > 0 && first_index + len > next_byte && first_index < next_byte + output.available_capacity()) {
-    for (uint64_t i=first_index; i<first_index + len; i++) {
-      if (char_pending[i] == INT_MIN) byte_pending++;
+    for (uint64_t i=first_index; i<min(first_index + len, next_byte + output.available_capacity()); i++) {
+      if (char_pending[i] == low_bound) byte_pending++;
       char_pending[i] = data[i-first_index];
       is_last[i] = is_last_substring;
     }
